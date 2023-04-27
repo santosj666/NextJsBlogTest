307f9db7a671fbd03c601a6acd99fd539f518c05
santosj666 <105174800+santosj666@users.noreply.github.com>
2023-04-27 23:06:53 +0100

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import "../styles/global.css"
+import {useRouter} from 'next/router'
+
+function App({ Component, pageProps }) {
+const router = useRouter();
+
+    return (
+        <>
+          <div>
+            <h1>Our site</h1>
+            <nav className="header-nav">
+                <ul>
+                    <li>
+                        <Link className={router.pathname == "/" ? "active" : ""} href="/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link className={router.pathname == "/about" ? "active" : ""} href="/about" >About</Link>
+                    </li> 
+                    <li>
+                        <Link className={router.pathname == "/blog" ? "active" : ""} href="/blog" >Blog</Link>
+                    </li>
+                </ul>
+            </nav>
+          </div>
+          <Component {...pageProps} />
+          <p>Footer</p>
+         </>
+    )
+}
+
+export default App;
